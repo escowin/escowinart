0260ca187cc1512cbc0fff1faa3efdcd10e305a3
escowin <edwin@escowinart.com>
2022-11-09 12:26:39 -0600

    set up ui interacivity logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
-let date = new Date().getFullYear();
+// dom
+const portfolioLinkEl = document.getElementById('portfolio-link');
+const aboutLinkEl = document.getElementById('about-link');
+const contactLinkEl = document.getElementById('contact-link');
 
-let currentYear = function() {
+// logic
+function clickedLink() {
+    console.log('hey')
+};
+
+function currentYear() {
+    let date = new Date().getFullYear();
+    console.log(`
+    \u00A9 ${date} Edwin M. Escobar
+    https://github.com/escowin/escowinart
+    `);
     const dateEl = document.querySelector("#date");
     dateEl.textContent = date; 
-}
+};
 
+// calls
+portfolioLinkEl.addEventListener('click', () => clickedLink());
+aboutLinkEl.addEventListener("click", () => clickedLink());
+contactLinkEl.addEventListener("click", () => clickedLink());
 currentYear();
