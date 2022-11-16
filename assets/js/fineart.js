e994ee6bd78c16e2cd23a1bce1623044f918e2c8
escowin <edwin@escowinart.com>
2022-11-15 18:31:32 -0600

    set up art array

---
+++ assets/js/fineart.js
@@ -0,0 +1,26 @@
+const art = [
+  {
+    set: "set-005 [ frequency ]",
+    medium: "oil on canvas",
+    year: 2021,
+    artwork: [
+      {
+        title: "« 003120210912 · r-g »",
+        length: 36,
+        width: 24,
+      },
+      {
+        title: "« 003220210913 · b-w »",
+        length: 36,
+        width: 24,
+      },
+      {
+        title: "« 003320210914 · y-b »",
+        length: 36,
+        width: 24,
+      },
+    ],
+  },
+];
+
+console.log(art[0].artwork);
