ab656afbf630058d29e7df30e3588df9ecd73583
escowin <edwin@escowinart.com>
2022-11-21 01:13:59 -0600

    dynamically populating data in ui, but see js:106 note

--- assets/js/fineart.js
+++ assets/js/fineart.js
@@ -2,6 +2,57 @@ const artworkEl = document.getElementById("artwork");
 
 const art = [
   {
+    set: "set-003",
+    subtitle: "thought",
+    medium: "oil on canvas",
+    year: 2021,
+    artworks: [
+      {
+        title: "002420210525 · closed-loop",
+        length: 36,
+        width: 24,
+      },
+      {
+        title: "002520210525 · interlinked",
+        length: 36,
+        width: 24,
+      },
+    ],
+  },
+  {
+    set: "set-004",
+    subtitle: "time",
+    medium: "oil on canvas",
+    year: 2021,
+    artworks: [
+      {
+        title: "002620210902 · onset",
+        length: 24,
+        width: 18,
+      },
+      {
+        title: "002720210902 · float",
+        length: 24,
+        width: 18,
+      },
+      {
+        title: "002820210902 · stare",
+        length: 24,
+        width: 18,
+      },
+      {
+        title: "002920210902 · seated",
+        length: 18,
+        width: 18,
+      },
+      {
+        title: "003020210902 · done",
+        length: 24,
+        width: 18,
+      },
+    ],
+  },
+  {
     set: "set-005",
     subtitle: "frequency",
     medium: "oil on canvas",
@@ -24,68 +75,12 @@ const art = [
       },
     ],
   },
-  // {
-  //   set: "set-004",
-  //   subtitle: "time",
-  //   medium: "oil on canvas",
-  //   year: 2021,
-  //   artworks: [
-  //     {
-  //       title: "002620210902 · onset",
-  //       length: 24,
-  //       width: 18,
-  //     },
-  //     {
-  //       title: "002720210902 · float",
-  //       length: 24,
-  //       width: 18,
-  //     },
-  //     {
-  //       title: "002820210902 · stare",
-  //       length: 24,
-  //       width: 18,
-  //     },
-  //     {
-  //       title: "002920210902 · seated",
-  //       length: 18,
-  //       width: 18,
-  //     },
-  //     {
-  //       title: "003020210902 · done",
-  //       length: 24,
-  //       width: 18,
-  //     },
-  //   ],
-  // },
 ];
 
 // console.log(sidescrollEl);
 
-function getDetails(artworks) {
-  console.log(artworks[0].title)
-  console.log(artworks[0].length)
-  console.log(artworks[0].width)
-
-
-  // console.log(container)
-  // const detailsEl = document.createElement("section");
-  // detailsEl.className = "artwork-details dark";
-
-  // for (let i = 0; i < artworks.length; i++) {
-  //   const titleEl = document.createElement("p");
-  //   titleEl.className = "artwork-title";
-  //   titleEl.innerText = `« ${artworks[i].title} »`;
-
-  //   const dimensionsEl = document.createElement("p");
-  //   dimensionsEl.className = "artwork-dimensions";
-  //   dimensionsEl.innerText = `${artworks[i].length} in &times; ${artworks[i].width} in`;
-
-  //   detailsEl.appendChild(titleEl);
-  //   detailsEl.appendChild(dimensionsEl);
-  // }
-}
-
-function getArtwork(art) {
+function getArtwork(ouvre) {
+  let art = ouvre.reverse();
   for (let i = 0; i < art.length; i++) {
     const containerEl = document.createElement("article");
     containerEl.setAttribute("id", art[i].set);
@@ -96,13 +91,25 @@ function getArtwork(art) {
         <p class="medium">${art[i].medium}</p>
       </section>
       <section class="artwork-container"></section>
-       ${getDetails(art[i].artworks)}
+      <section class="artwork-details dark">${getDetails(
+        art[i].artworks
+      )}</section>
     `;
 
     sidescrollEl.appendChild(containerEl);
-    console.log(containerEl);
-    // console.log(art[i].artworks[i])
   }
 }
+
+function getDetails(details) {
+  let arr = [];
+  let result;
+  // ned to remove ',' before display
+  for (let i = 0; i < details.length; i++) {
+    result = `<p class='artwork-title'>« ${details[i].title} »</p><p class='artwork-dimensions'>${details[i].length} in &times; ${details[i].width} in</p>`;
+    arr.push(result)
+  }
+  console.log(arr)
+  return arr
+};
 
 getArtwork(art);
