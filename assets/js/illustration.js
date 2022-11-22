2060cf6b9d5c56a0f20f2747d75c20f6669b9f18
escowin <edwin@escowinart.com>
2022-11-21 23:49:03 -0600

    iterating through img array

--- assets/js/illustration.js
+++ assets/js/illustration.js
@@ -1,15 +1,18 @@
+// data | global dom
+const mainEl = document.getElementById('illustration-portfolio');
+
 // data | array of illustrations sorted by series, subdivided by volume
 const illustrations = [
-  {
-    series: "bluestrawberry",
-    volumes: [
-      {
-        title: "vol 1",
-        id: "bluestraberry-001",
-        img: ["./assets/images/artwork/illustration/bluestrawberry/.jpg"],
-      },
-    ],
-  },
+//   {
+//     series: "bluestrawberry",
+//     volumes: [
+//       {
+//         title: "vol 1",
+//         id: "bluestraberry-001",
+//         img: ["./assets/images/artwork/illustration/bluestrawberry/.jpg"],
+//       },
+//     ],
+//   },
   {
     series: "days",
     volumes: [
@@ -61,22 +64,22 @@ const illustrations = [
         title: "05 | 0021-0025",
         id: "days-05",
         img: [
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210330-0021.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210331-0022.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210401-0023.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210402-0024.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210403-0025.jpg",
         ],
       },
       {
         title: "06 | 0026-0030",
         id: "days-06",
         img: [
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210425-0031.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210426-0032.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210427-0033.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210428-0034.jpg",
-          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210429-0035.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210420-0026.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210421-0027.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210422-0028.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210423-0029.jpg",
+          "./assets/images/artwork/illustration/days/edwin-escobar-days-20210424-0030.jpg",
         ],
       },
       {
@@ -94,18 +97,57 @@ const illustrations = [
   },
 ];
 
-// console.log(illustrations[0].volumes);
-
 // logic | gets illustrations array data
 function getIllustrations(illustrations) {
   // the last series will display first
-  let series = illustrations.reverse();
+  let seriesArr = illustrations.reverse();
 
   // each .section will correspond with each object in the series array
-  for (let i = 0; i < series.length; i++) {
-    console.log(series[i]);
+  for (let i = 0; i < seriesArr.length; i++) {
+    let title = seriesArr[i].series;
+    const titleEl = document.createElement('h3');
+    titleEl.className = "series dark";
+    titleEl.innerText = `[ ${title} ]`
+
+    const x = getDetails(seriesArr[i].volumes);
+
+    // console.log(x)
+
+    mainEl.appendChild(titleEl);
+    mainEl.appendChild(x);
   }
-}
+//   console.log(mainEl)
+};
+
+function getDetails(volumes) {
+    let detailsArr = volumes.reverse();
+
+    for (let i = 0; i < detailsArr.length; i++) {
+        let subheaderEl = document.createElement('h4');
+        subheaderEl.className = "artwork-header dark";
+        subheaderEl.innerText = `« ${detailsArr[i].title} »`;
+
+        let sidescrollEl = document.createElement('div');
+        sidescrollEl.className = "horizontal-wrapper dark sidescroll";
+        sidescrollEl.setAttribute('id', detailsArr[i].id)
+        sidescrollEl.innerHTML = `
+        <article class="container">
+            <section class="artwork-container">
+                ${displayIllustrations(detailsArr[i].img)}
+            </section>
+        </article>`;
+
+        const containerEl = document.createElement('article');
+        containerEl.className = "container";
+    }
+};
+
+function displayIllustrations(imgArr) {
+    // console.log(imgArr);
+    for (let i = 0; i < imgArr.length; i++) {
+        console.log(imgArr[i])
+    }
+};
 
 // calls
 getIllustrations(illustrations);
