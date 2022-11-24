b24766f904e93e37ecdbf7b34e392b804dbcaece
escowin <edwin@escowinart.com>
2022-11-24 14:19:57 -0600

    dynamically generating graphic design portfolio

--- assets/js/graphicdesign.js
+++ assets/js/graphicdesign.js
@@ -1,6 +1,4 @@
-const graphicsContainerEl = document.getElementById('graphics-container')
-console.log(graphicsContainerEl);
-
+// data | each array object holds a unqieu name & img url
 const portfolio = [
     {
         name: "id",
@@ -16,14 +14,32 @@ const portfolio = [
     }
 ];
 
+// logic | displays portfolio
 function displayPortfolio(graphicdesign) {
-    // console.log(graphicdesign);
-    let template;
+    // dom element to hold master template
+    const graphicsContainerEl = document.getElementById('graphics-container');
+    let htmlArr = [];
+
+    // iterates through the array, each index pushes a template literal into the master html array
     for (let i = 0; i < graphicdesign.length; i++) {
-        let result = `<article></article>`
-        // console.log(graphicdesign[i])
-        console.log(result)
+        let result = `
+        <article class="container">
+            <h3 class="dark graphic-header shadow">${graphicdesign[i].name}</h3>
+            <div class="artwork-container">
+                <img src="${graphicdesign[i].img}"/>
+            </div>
+        </article>`;
+        
+        htmlArr.push(result);
     }
+
+    // converts html array into a string, removing the comma between objects. this string is the inner html of graphics container
+    let template = htmlArr.toString().replace(/>,/g, '>');
+    graphicsContainerEl.innerHTML = template;
+
+    // final output
+    return graphicsContainerEl;
 };
 
-displayPortfolio(portfolio)
+// call
+displayPortfolio(portfolio);
