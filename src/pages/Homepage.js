fcac598c7a588136de8fa20d6264c07ba482a037
escowin <edwin@escowinart.com>
2024-02-16 18:56:36 -0600

    refactored homepage JSX through helpers & array iteration

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import "../assets/css/homepage.css";
 import { portfolio, format } from "../utils/helpers";
+import "../assets/css/homepage.css";
 
 function Homepage() {
-  console.log(portfolio.filter(key => !key.includes("about")))
+  const portfolios = portfolio.filter((key) => !key.includes("about"));
 
-  const portfolioImages = {
+  const images = {
     fineart: [
       "edwin-escobar-set-001.jpg",
       "edwin-escobar-set-002.jpg",
@@ -27,14 +27,14 @@ function Homepage() {
       // "days/edwin-escobar-days-20210420-0026.jpg",
       // "row/edwin-escobar-2023-001_001.jpg",
       // "city-life/edwin-escobar-20170301-nat-sool.jpg",
-      "days/edwin-escobar-days-20210306-0002.jpg"
+      "days/edwin-escobar-days-20210306-0002.jpg",
     ],
     graphicDesign: [
       // "edwin-escobar-20210225-made-in-a-lab.png",
       // "edwin-escobar-20220515-cool-with-coolidge.jpg",
-      // "edwin-escobar-20220515-stop-being-poor.png",
+      "edwin-escobar-20220515-stop-being-poor.png",
       // "edwin-escobar-20210220-capitalism.png",
-      "edwin-escobar-20220515-mindlessly-parrot-social-media.png",
+      // "edwin-escobar-20220515-mindlessly-parrot-social-media.png",
       // "Edwin Escobar 20211021 ACE Posters_CPR.png",
       // "Edwin Escobar 20211021 ACE Posters_History.png",
       // "Edwin Escobar 20211021 ACE Posters_Math.png",
@@ -42,17 +42,17 @@ function Homepage() {
     ],
   };
 
-
-  const randomFineArt =
-    portfolioImages.fineart[Math.floor(Math.random() * portfolioImages.fineart.length)];
-  const randomIllustration =
-    portfolioImages.illustration[
-      Math.floor(Math.random() * portfolioImages.illustration.length)
-    ];
-  const randomGraphicDesign =
-    portfolioImages.graphicDesign[
-      Math.floor(Math.random() * portfolioImages.graphicDesign.length)
-    ];
+  const randomImage = {
+    fineart: images.fineart[Math.floor(Math.random() * images.fineart.length)],
+    illustration:
+      images.illustration[
+        Math.floor(Math.random() * images.illustration.length)
+      ],
+    graphicDesign:
+      images.graphicDesign[
+        Math.floor(Math.random() * images.graphicDesign.length)
+      ],
+  };
 
   let suffix = "";
   if (window.matchMedia("(min-width: 1024px)").matches) {
@@ -68,56 +68,29 @@ function Homepage() {
 
   return (
     <section className="section" id="homepage">
-      {portfolio.map((art, i) => (
-        <article key={i} className="portfolio" id={`random-${format.kebab(art)}`}>
+      {portfolios.map((art, i) => (
+        <article key={i} className="portfolio">
           <Link to={`/${format.kebab(art)}`}>
-            <img className="random-img" alt={format.kebab(art)} src={require(`../assets/images/${art}/${randomFineArt}`)} />
-            <p>{format.kebab(art)}</p>
+            <img
+              className="portfolio-img"
+              alt={format.sentence(art)}
+              src={require(`../assets/images/${format.kebab(art)}/${
+                randomImage[art]
+              }`)}
+            />
+            <p>{format.sentence(art)}</p>
           </Link>
         </article>
       ))}
-      <article className="portfolio" id="random-fineart">
-        <Link to={"/fine-art"}>
-          <img
-            className="random-img"
-            alt="fine art"
-            src={require(`../assets/images/fineart/${randomFineArt}`)}
-          />
-          <p>Fine art</p>
-        </Link>
-      </article>
-
-      <article className="portfolio" id="random-illustration">
-        <Link to={"/illustration"}>
-          <img
-            className="random-img"
-            alt="illustration"
-            src={require(`../assets/images/illustration/${randomIllustration}`)}
-          />
-          <p>Illustration</p>
-        </Link>
-      </article>
-
-      <article className="portfolio" id="random-graphic">
-        <Link to={"/graphic-design"}>
-          <img
-            className="random-img"
-            alt="graphic design"
-            src={require(`../assets/images/graphic-design/${randomGraphicDesign}`)}
-          />
-          <p>Graphic design</p>
-        </Link>
-      </article>
 
       <article
         className="portfolio"
-        id="software"
         onClick={() =>
           window.open("https://escowin.github.io/portfolio", "_blank")
         }
       >
         <img
-          className="random-img"
+          className="portfolio-img"
           alt="software"
           src={require(`../assets/images/software/portfolio${suffix}.jpg`)}
         />
